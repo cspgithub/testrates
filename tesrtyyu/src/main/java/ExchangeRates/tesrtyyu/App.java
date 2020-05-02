e6bbe0b532284d9d66365fac9b4367bcf4914d95
cspgithub <cspandey3000@gmail.com>
2020-05-02 11:37:08 +0530

    hgfj

---
+++ tesrtyyu/src/main/java/ExchangeRates/tesrtyyu/App.java
@@ -0,0 +1,13 @@
+package ExchangeRates.tesrtyyu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
