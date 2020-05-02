b497e7533e8d9a05329c883560aeb0fe5040e031
cspgithub <cspandey3000@gmail.com>
2020-05-02 12:29:13 +0530

    comments updated

--- tesrtyyu/src/test/java/stepdefinitions/GetRequestStepDefinitions.java
+++ tesrtyyu/src/test/java/stepdefinitions/GetRequestStepDefinitions.java
@@ -24,23 +24,32 @@ public class GetRequestStepDefinitions extends TestBase {
 
 	@When("I hit the api with get request and end point as {string}")
 	public void i_hit_the_api_with_get_request_and_end_point_as(String endPoint) {
+		//URL details will be printed in test report
+		testContext.scn.write( server+endPoint );
 		testContext.resp = testContext.req_spec.when().get(endPoint);
+		//Response details will be printed in test report
 		testContext.scn.write("Response of the request is: " + testContext.resp.asString() + "<br>");
 	}
 
 	@Then("API returned the status code as {int}")
 	public void api_returned_the_status_code_as(Integer statusMsg) {
 		testContext.resp.then().assertThat().statusCode(statusMsg);
+		
+		testContext.scn.write("Validated the status code,status code is : " + statusMsg + "<br>");
 	}
 
 	@Then("base value should be {string}")
 	public void base_value_should_be(String baseValue) {
+		
 		testContext.resp.then().assertThat().body("base", equalTo(baseValue));
+		testContext.scn.write("Validated the base value,base value is : " + baseValue + "<br>");
 	}
 
 	@Then("error message displayed as {string}")
 	public void error_message_displayed_as(String errorMssg) {
 		testContext.resp.then().assertThat().body("error", equalTo(errorMssg));
+		testContext.scn.write("Validated the error message,error message content is : " + errorMssg + "<br>");
+		
 	}
 
 	@Then("API should return current date rates")
